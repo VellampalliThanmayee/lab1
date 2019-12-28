c648304a3bbf4a7c2762f3ae9b7a2b9b7572da5c
VellampalliThanmayee <59306833+VellampalliThanmayee@users.noreply.github.com>
2019-12-28 15:11:23 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,14 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:57:15) [MSC v.1915 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+# Python program to find largest 
+# number in a list 
+  
+# list of numbers 
+list1 = [10, 20, 4, 45, 99] 
+  
+# sorting the list 
+list1.sort() 
+  
+# printing the last element 
+print("Largest element is:", list1[-1]) 
